3d0e9867826dce87f22bbd56429cdca6b82f861c
yuseonhwa <dbtjsghk21@naver.com>
2024-01-31 23:24:06 +0900

    forEach

--- Problems/forEachMap/solve.js
+++ Problems/forEachMap/solve.js
@@ -1,4 +1,15 @@
-// write your codes
-function solution(inputArray) {}
+
+const mdn = 'https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach'
+function solution(inputArray) {
+    const arr = []
+    /**
+     * 1.  forEach는 순회 메서드다.
+     * 2.  map 과 달리 forEach()는 항상 undefined를 반환하므로 체이닝이 불가하다.
+     */
+    inputArray.forEach((item)=> arr.push(item +'%'))
+    return arr
+}
+const answer =  solution([100, 10, 20, 40])
+console.log(answer)
 
 exports.solution = solution;
