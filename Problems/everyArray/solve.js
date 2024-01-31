8e095c2c8ef646c391ba9d8b50213eb2d0d8b59e
yuseonhwa <dbtjsghk21@naver.com>
2024-01-31 20:25:16 +0900

    every 풀이

--- Problems/everyArray/solve.js
+++ Problems/everyArray/solve.js
@@ -1,3 +1,12 @@
-function solution(inputArray) {}
+const mdnUrl = 'https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/every'
+function solution(inputArray) {
+    console.log(inputArray)
+    const newArr = []
+    const isBelowThreshold = (currentValue) => currentValue % 2 === 0;
 
+    return inputArray.every(isBelowThreshold)
+
+}
+const answer = solution([2,4,6,8])
+console.log(answer)
 exports.solution = solution;
