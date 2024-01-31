00a0d06d9d9fccaf971daa6b6876b3f71ca093ac
yuseonhwa <dbtjsghk21@naver.com>
2024-01-31 23:11:30 +0900

    exp

--- Problems/expDivOdd/solve.js
+++ Problems/expDivOdd/solve.js
@@ -1,3 +1,13 @@
-function solution(inputArray) {}
-
+function solution(inputArray) {
+    let cnt = 0
+    for(let i =0;i<inputArray.length;i++){
+        const ans = Math.pow(inputArray[i],2)%3
+        if(ans%2 !== 0){
+            cnt += Math.pow(inputArray[i],2)
+        }
+    }
+    return cnt
+}
+const answer = solution([1, 7, 3, 4, 6])
+console.log(answer)
 exports.solution = solution;
